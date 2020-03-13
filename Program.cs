a2e86bd9aa46893287bf6e188948070416822ac5
Artyom Shukanov <a.shukanov@hqsoftwarelab.com>
2020-03-13 20:46:46 +0300

    initial commit

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace uloc
+{
+    /// Reference article:
+    /// http://anthonygiretti.com/2018/01/24/code-quality-using-codeanalysis-fxcopanalyzers-and-stylecop-in-net-core-2-applications/
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var t = 1;
+        }
+    }
+}
