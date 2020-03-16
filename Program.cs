2fbad8fcdf99099a1d0d677e6df4393ece397fad
Artyom Shukanov <a.shukanov@hqsoftwarelab.com>
2020-03-16 09:06:16 +0300

    Add reference link

--- Program.cs
+++ Program.cs
@@ -2,8 +2,12 @@
 
 namespace uloc
 {
-    /// Reference article:
-    /// http://anthonygiretti.com/2018/01/24/code-quality-using-codeanalysis-fxcopanalyzers-and-stylecop-in-net-core-2-applications/
+    /// Reference articles:
+    /// Microsoft Managed Recommended Rules: 
+    ///     https://docs.microsoft.com/en-us/visualstudio/code-quality/managed-recommended-rules-rule-set-for-managed-code?view=vs-2019
+    /// Anthony Girettib Blog Post:
+    ///     http://anthonygiretti.com/2018/01/24/code-quality-using-codeanalysis-fxcopanalyzers-and-stylecop-in-net-core-2-applications/
+    /// 
     class Program
     {
         static void Main(string[] args)
